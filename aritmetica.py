986c5cfe145ffab7a699967812c08a1d147e5592
NEBULA1978 <rpascualey@gmail.com>
2023-01-16 01:19:27 +0100

    Converion libras a euros terminado

---
+++ aritmetica.py
@@ -0,0 +1,54 @@
+# unos ejemplos de aritmetica con python
+
+# Aquí hay algunos ejemplos de operaciones aritméticas básicas en Python:
+
+# Suma:
+
+x = 5
+y = 3
+result = x + y
+print(result)  # imprime 8
+
+# Resta:
+
+x = 5
+y = 3
+result = x - y
+print(result)  # imprime 2
+
+# Multiplicación:
+
+x = 5
+y = 3
+result = x * y
+print(result)  # imprime 15
+
+# División:
+
+x = 5
+y = 3
+result = x / y
+print(result)  # imprime 1.6666666666666667
+
+# Exponenciación:
+
+x = 5
+y = 3
+result = x ** y
+print(result)  # imprime 125
+
+# División entera:
+
+x = 5
+y = 3
+result = x // y
+print(result)  # imprime 1
+
+# Módulo:
+
+x = 5
+y = 3
+result = x % y
+print(result)  # imprime 2
+
+# Es importante tener en cuenta que Python también admite operaciones aritméticas con números flotantes y complejos.
---
+++ input-output.py
@@ -0,0 +1,39 @@
+# Ejemplos de input y optputs en python
+
+# Sí, aquí hay algunos ejemplos de cómo utilizar la función input() y cómo mostrar salida con print() en Python:
+
+# Ejemplo de input:
+
+nombre = input("Ingresa tu nombre: ")
+print("Hola, " + nombre + "!")
+
+# Salida:
+
+# Ingresa tu nombre: Juan
+# Hola, Juan!
+
+# Ejemplo de output:
+
+x = 5
+y = 10
+suma = x + y
+print("La suma de", x, "y", y, "es igual a", suma)
+
+# Salida:
+
+# La suma de 5 y 10 es igual a 15
+
+# Ejemplo de input y output
+
+num1 = int(input("Ingresa un número: "))
+num2 = int(input("Ingresa otro número: "))
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es igual a", suma)
+
+# Salida:
+
+# Ingresa un número: 5
+# Ingresa otro número: 10
+# La suma de 5 y 10 es igual a 15
+
+# Ten en cuenta que la función input() siempre devuelve una cadena, por lo que si necesitas trabajar con números, debes convertirlos a un tipo numérico(como int o float) antes de hacer cualquier operación matemática con ellos.
